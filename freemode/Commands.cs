8cb2adb87819fd9e95e09c05706b61e5060ba2ff
matthew.kalashnikau2002 <matthewkalashnikov2002@gmail.com>
2021-12-05 17:59:51 +0300

    Add project files.

---
+++ freemode/Commands.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using GTANetworkAPI;
+
+namespace freemode
+{
+    class Commands : Script
+    {
+        [Command("veh", "/veh спавнит авто в координатах игрока", Alias ="vehicle")]
+        private void cmd_veh(Player player, string vehname, int color1, int color2)
+        {
+            uint vhash = NAPI.Util.GetHashKey(vehname);
+            if(vhash <= 0)
+            {
+                player.SendChatMessage("~r~Неверная модель т/с");
+            }
+            Vehicle veh = NAPI.Vehicle.CreateVehicle(vhash, player.Position, player.Heading, color1, color2);
+            veh.NumberPlate = "Matthew";
+            veh.Locked = false;
+            veh.EngineStatus = true;
+            player.SetIntoVehicle(veh, (int)VehicleSeat.Driver);
+        }
+
+        [Command("freeze", "/freeze [ник игрока] [true/false]")]
+        private void cmd_freezeplayer(Player player, Player target, bool freezestatus)
+        {
+            NAPI.ClientEvent.TriggerClientEvent(target, "PlayerFreeze", freezestatus);
+        }
+    }
+}
---
+++ freemode/Events.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using GTANetworkAPI;
+
+namespace freemode
+{
+    class Events : Script
+    {
+        [ServerEvent(Event.ResourceStart)]
+        public void OnResourceStarted()
+        {
+            mysql.InitConnection();
+        }
+        [ServerEvent(Event.PlayerConnected)]
+        private void OnPlayerConnected(Player player)
+        {
+            player.SendChatMessage("Добро пожаловать на сервер ~g~Global RP");
+            if(mysql.IsAccountExist(player.Name))
+            {
+                player.SendChatMessage("~w~Ваш аккаунт уже ~g~зарегистрирован~w~ на сервере. Используйте /login для авторизации.");
+            }
+            else
+            {
+                player.SendChatMessage("~w~Ваш аккаунт не ~g~зарегистрирован~w~ на сервере. Используйте /register для регистрации.");
+            }
+        }
+
+        [ServerEvent(Event.PlayerSpawn)]
+        private void OnPlayerSpawn(Player player)
+        {
+            player.Health = 50;
+            player.Armor = 50;
+        }
+    }
+}
